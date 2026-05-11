af87ee44caf92d2b9083b2c1dcbf621e4f5a5f30
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-05-11 13:12:47 +0530

    add

--- 9_ReactHookFrom/src/Form.jsx
+++ 9_ReactHookFrom/src/Form.jsx
@@ -4,11 +4,59 @@ import { useForm } from "react-hook-form"
 
 function Form(){
 
-    const {register,handleSubmit} = useForm();
+    const {register,handleSubmit,formState:{errors}} = useForm();//error for handle the error
 
+    function submitForm(data){
+        console.log(data);
+    }
+
+    console.log("Render");
     return(
         <>
-        
+        <form onSubmit={handleSubmit(submitForm)}>
+        <div>
+            <label htmlFor="first">Name:</label>
+            <input id="first" {...register("name",
+                {required:"Name is required"}
+            )} />
+            {errors.name && <span>{errors.name.message}</span>}
+        </div>
+        <div>
+            <label htmlFor="second">Age:</label>
+            <input id="second" {...register("age",
+                {
+                    min:{
+                    value:18,
+                    message:"Age must be at least 18"
+                    },
+                    max:{
+                    value:80,
+                    message:"Age must be less than 80"
+                    },
+                    required:"Age is required"
+                }
+            )} />
+            {errors.age && <span>{errors.age.message}</span>}
+        </div>
+        <div>
+            <label htmlFor="third">Password:</label>
+            <input type="password" id="third" {...register("password",
+            {
+                    minLength:{
+                    value:5,
+                    message:"Password must be at least 5 characters"
+                    },
+                    maxLength:{
+                    value:10,
+                    message:"Password must be less than 10 characters"
+                    },
+                    required:"Password is required"
+                }
+            )} />
+            {errors.password && <span>{errors.password.message}</span>}
+        </div> 
+        <button>Submit</button>
+        </form>
         </>
     )
 }
